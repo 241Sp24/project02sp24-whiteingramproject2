2221317fe096f0bc821b36b42ca57e74749a3ee0
TylerIngman <160711678+TylerIngman@users.noreply.github.com>
2024-04-10 05:37:49 -0500

    Update OnlineStudent.java

--- src/studentdriver/OnlineStudent.java
+++ src/studentdriver/OnlineStudent.java
@@ -1,1 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StudentDriver;
 
+/**
+ *
+ * @author S564997
+ */
+public class OnlineStudent extends StudentFees{
+    private int noOfMonths;
+    private double MONTHLY_FEE = 1245.25;
+    
+    public OnlineStudent(int studentID, String studentName, boolean isEnrolled, int noOfMonths){
+        super(studentName, studentID, isEnrolled);
+        this.noOfMonths = noOfMonths;
+    }
+    
+    public double getPayableAmount(){
+        return noOfMonths * MONTHLY_FEE;
+    }
+    
+    public String toString(){
+        return "Student name: " + super.getStudentName() + "\nStudent id: " + super.getStudentID() + "\nEnrolled: " + isIsEnrolled() + "\nNo of months: " + noOfMonths + "\nPayable amount: " + getPayableAmount() + "\n";
+    }
+}
